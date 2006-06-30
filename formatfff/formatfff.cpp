240b047a5165da91b7f4bdac00c39cf78dce0092
rasmus <rasmus>
2006-06-30 17:14:04 +0000

    *** empty log message ***

--- formatfff/formatfff.cpp
+++ formatfff/formatfff.cpp
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
+
+using namespace std;
 
 
-int main(int argc, char **argv)
+bool indexFff(string filename, int nstarts)
 {
     FILE *infile;
+    FILE *outfile;
+
+    // parsing variables
     char name[51];
     char species[51];
     char chrom[51];
@@ -12,23 +18,29 @@ int main(int argc, char **argv)
     int len;
     char comment;
     
-    // number of feature per index line
-    int nstarts = 1000;
     
-    // check args
-    if (argc < 2) {
-        fprintf(stderr, "usage: formatfff <fff file>\n");
-        exit(1);
+    // ensure correct filename names and construct output filename
+    if (filename.substr(filename.size() - 4, 4) != ".fff") {
+        fprintf(stderr, "formatfff: input file does not end with '.fff'\n");
+        return false;
+    }
+    string outfilename = filename;
+    outfilename[outfilename.size() - 1] = 'i';  // make new file a '*.ffi'
+    
+    
+    // open input and output files
+    if ((infile = fopen(filename.c_str(), "r")) == NULL) {
+        fprintf(stderr, "formatfff: cannot open file '%s'\n", filename.c_str());
+        return false;
     }
     
-    // open input file
-    if ((infile = fopen(argv[1], "r")) == NULL) {
-        fprintf(stderr, "cannot open file '%s'\n", argv[1]);
-        exit(1);
+    if ((outfile = fopen(outfilename.c_str(), "w")) == NULL) {
+        fprintf(stderr, "formatfff: cannot open output file '%s'\n", outfilename.c_str());
+        return false;
     }
     
     
-    
+    // process file
     while (!feof(infile)) {
         // check for comments
         comment = fgetc(infile);
@@ -50,20 +62,76 @@ int main(int argc, char **argv)
         fscanf(infile, "%50[^\t]\t%50[^\t]\t%50[^\t]\t%d%*[\t\n]", 
                name, species, chrom, &len);
         
+        // parse starts
         int i = nstarts;
         int pos = ftell(infile);
-        while (fscanf(infile, " %50[-0-9]", startstr) == 1) {
+        int start2;
+        int last = 0;
+        while (fscanf(infile, "\t%50[-0-9]", startstr) == 1) {
             i++;
             
+            // check for start sorting
+            start2 = abs(atoi(startstr));
+            if (start2 < last) {
+                fprintf(stderr, "formatfff: feature starts are not sorted!\n");
+                return false;
+            }
+            last = start2;
+            
+            
             if (i >= nstarts) {
+                // output file position to index file
                 int start = abs(atoi(startstr));
-                printf("%s\t%s\t%s\t%d\t%d\t%d\n", 
-                       name, species, chrom, len, start, pos);
+                fprintf(outfile, "%s\t%s\t%s\t%d\t%d\t%d\n", 
+                        name, species, chrom, len, start, pos);
                 i = 0;
             }
             
+            // get file position
             pos = ftell(infile);
         }
     }
+    
+    // clean up
+    fclose(infile);
+    fclose(outfile);
+    
+    return true;
+}
+
+
+int main(int argc, char **argv)
+{
+    // number of feature per index line
+    int nstarts = 1000;
+    
+    
+    // check args
+    if (argc < 2) {
+        fprintf(stderr, "usage: formatfff [-n indexsize] <fff file>\n");
+        exit(1);
+    }
+    
+    // parse args
+    int i;
+    for (i=1; i<argc; i++) {
+        if (argv[i] == "-n") {
+            nstarts = atoi(argv[++i]);
+        } else {
+            // this must be a fff filename
+            break;
+        }
+    }
+    
+    // process fff files
+    for (; i<argc; i++) {
+        if (!indexFff(argv[i], nstarts)) {
+            fprintf(stderr, "formatfff: error processing '%s'\n", argv[i]);
+            exit(1);
+        }
+        fprintf(stderr, "processed '%s'\n", argv[i]);
+    }
+    
+    return 0;
 }
 

db28729126f3009aa9d5b68d2748117c7a596dfc
raz <raz>
2006-09-06 02:18:34 +0000

    *** empty log message ***

--- bin/lineno.py
+++ bin/lineno.py
@@ -2,6 +2,15 @@
 
 import sys
 
+if len(sys.argv) > 1:
+    width = int(sys.argv[1])
+    patt = "%%%dd  " % width
+else:
+    patt = "%d  "
+
 i = 0
 for line in sys.stdin:
-    print i, line,
+    i += 1
+    sys.stdout.write(patt % i)
+    sys.stdout.write(line)
+    sys.stdout.flush()

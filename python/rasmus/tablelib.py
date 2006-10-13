30932b554bc10bbe233075a763ba03904383620b
rasmus <rasmus>
2006-10-13 17:57:30 +0000

    *** empty log message ***

--- python/rasmus/tablelib.py
+++ python/rasmus/tablelib.py
@@ -4,15 +4,16 @@ tables.py
 Implements Manolis style tab-delimited table file format.
 
 
-##delim:whitespace,space,tab,',',';'
+##version:1.0
 ##types:string int
 ##default:'none' 0
-##expect:
-##author:
 ##header:1
 #
 #
 #
+##expect:
+##author:
+##delim:whitespace,space,tab,',',';'
 #
 #
 #
@@ -32,11 +33,14 @@ import util
 
 
 # table directives
-DIR_TYPES    = 0
-DIR_HEADERS  = 1
-DIR_DEFAULTS = 2
+DIR_VERSION  = 0
+DIR_TYPES    = 1
+DIR_HEADERS  = 2
+DIR_DEFAULTS = 3
 #DIR_DELIM    = 3
 
+# a special null type 
+NULL = util.Bundle()
 
 
 class Table (list):
@@ -54,6 +58,7 @@ class Table (list):
         self.comments = []
         self.delim = "\t"
         self.nheaders = 1
+        self.version = "1.0"
         
         
         # set data
@@ -134,6 +139,9 @@ class Table (list):
         self.comments = []
         self.delim = delim
         self.nheaders = 1
+        self.version = "1.0"
+        headerskip = 0
+        
         
         # temps for reading only
         self.tmptypes = None
@@ -160,7 +168,12 @@ class Table (list):
             if not self.headers:
                 # parse header
                 if self.nheaders > 0:
-                    self.headers = tokens
+                    if headerskip >= self.nheaders - 1:
+                        self.headers = tokens
+                    else:
+                        self.comments.append(line)
+                        headerskip += 1
+                        continue
                 else:
                     self.headers = range(len(tokens))
                 
@@ -205,6 +218,8 @@ class Table (list):
     
     
     def write(self, filename, delim="\t"):
+        self.filename = filename
+    
         out = util.openStream(filename, "w")
         
         # ensure all info is complete
@@ -218,10 +233,15 @@ class Table (list):
             if key not in self.defaults:
                 self.defaults[key] = self.types[key]()
             
-        
+                    
         # ensure types are in directives
         if DIR_TYPES not in self.comments:
             self.comments = [DIR_TYPES] + self.comments
+        
+        # ensure version is in directives
+        if DIR_VERSION not in self.comments:
+            self.comments = [DIR_VERSION] + self.comments
+
 
         # write comments
         for line in self.comments:
@@ -237,7 +257,13 @@ class Table (list):
 
         # write data
         for row in self:
-            print >>out, delim.join(map(lambda x: str(row[x]), self.headers))
+            rowstr = []
+            for header in self.headers:
+                if header in row:
+                    rowstr.append(str(row[header]))
+                else:
+                    rowstr.append('')
+            print >>out, delim.join(rowstr)
     
     
     def save(self):
@@ -250,9 +276,11 @@ class Table (list):
         
     
     def determineDirective(self, line):
-        if line.startswith("#Types:") or \
-           line.startswith("#types:") or \
-           line.startswith("##types:"):
+        if line.startswith("##version:"):
+            return DIR_VERSION
+        elif line.startswith("#Types:") or \
+             line.startswith("#types:") or \
+             line.startswith("##types:"):
             # backwards compatible
             return DIR_TYPES
             
@@ -274,10 +302,14 @@ class Table (list):
         if directive == None:
             return False
         
-        rest = line[line.index(":")+1:]
+        rest = line[line.index(":")+1:]         
         self.comments.append(directive)
         
-        if directive == DIR_TYPES:
+        if directive == DIR_VERSION:
+            self.version = rest
+            return True
+            
+        elif directive == DIR_TYPES:
             self.tmptypes = parseTableTypes(rest, self.delim)
             return True
             
@@ -287,8 +319,8 @@ class Table (list):
             
         elif directive == DIR_HEADERS:
             self.nheaders = int(rest)
-            if self.nheaders not in [0, 1]:
-                raise "Only 0 or 1 headers are allowed"
+            #if self.nheaders not in [0, 1]:
+            #    raise "Only 0 or 1 headers are allowed"
             return True
         
         else:
@@ -298,7 +330,10 @@ class Table (list):
     def writeDirective(self, line, out, delim):
         """Write a directive"""
         
-        if line == DIR_TYPES:
+        if line == DIR_VERSION:
+            out.write("##version:%s\n" % self.version)
+        
+        elif line == DIR_TYPES:
             entry = self[0]
             out.write("##types:" +
                       formatTableTypes(util.mget(self.types,
@@ -310,7 +345,7 @@ class Table (list):
                                 util.mget(self.defaults, self.headers))) + "\n")
         
         elif line == DIR_HEADERS:
-            out.write("##header:%d\n" % self.nheaders)
+            out.write("##headers:%d\n" % self.nheaders)
         
         else:
             raise "unknown directive:", line
@@ -318,7 +353,33 @@ class Table (list):
 
     
     def add(self, **kargs):
-        self.append(kargs)    
+        """Add a row to the table
+           
+           tab.add(col1=val1, col2=val2, col3=val3)
+        """
+        self.append(kargs)
+    
+    
+    def addCol(self, header, coltype=str, default=NULL, pos=None):
+        """Add a column to the table
+        
+           header  - name of the column
+           coltype - type of the values in that column
+           default - default value of the column
+           pos     - position to insert column (default: right-end)
+        """
+        
+        if header in self.headers:
+            raise Exception("header '%s' is already in table" % header)
+        
+        if pos == None:
+            pos = len(self.headers)
+        if default == NULL:
+            default = coltype()
+        
+        self.headers.insert(pos, header)
+        self.types[header] = coltype
+        self.defaults[header] = default
     
     
     def lookup(self, *keys):
@@ -339,19 +400,8 @@ class Table (list):
         lookup.insert = False
         return lookup
     
-    
-    def getMatrix(self):
-        """returns a copy of the table as a 2D list"""
-        
-        mat = [self.headers]
-        
-        for row in self:
-            mat.append(util.mget(row, self.headers))
-        
-        return mat
-    
-    
     def filter(self, cond):
+        """Returns a table with a subset of rows such that cond(row) == True"""
         tab = self.new()
         
         for row in self:
@@ -362,7 +412,7 @@ class Table (list):
     
     
     def get(self, rows=None, cols=None):
-        """get a table with a subset of the rows and columns"""
+        """Returns a table with a subset of the rows and columns"""
         
         tab = self.new()
         
@@ -388,9 +438,36 @@ class Table (list):
         return tab
     
     
-    def sort(self, cmp=cmp, key=None, reverse=False, field=None):
-        if field != None:
-            key = lambda row: row[field]        
+    def cget(self, *cols):
+        """Returns columns of the table as separate lists"""
+        
+        ret = []
+        
+        for col in cols:
+            newcol = []
+            ret.append(newcol)
+            
+            for row in self:
+                newcol.append(row[col])
+        
+        return ret
+    
+    
+    
+    def getMatrix(self):
+        """Returns a copy of the table as a 2D list"""
+        
+        mat = [self.headers]
+        
+        for row in self:
+            mat.append(util.mget(row, self.headers))
+        
+        return mat
+    
+    
+    def sort(self, cmp=cmp, key=None, reverse=False, col=None):
+        if col != None:
+            key = lambda row: row[col]        
         list.sort(self, cmp=cmp, key=key, reverse=reverse)
 
 
@@ -485,13 +562,7 @@ if __name__ == "__main__":
     import StringIO
     
     text="""\
-#
-#
 ##types:str	int	int
-##defaults:none	0	0
-#
-# hello
-#
 name	num	num2
 matt	123	3
 alex	456	
@@ -499,14 +570,14 @@ mike	789	1
 """
 
     tab = readTable(StringIO.StringIO(text))
+    tab.addCol("extra", bool, True)
     
     print tab.defaults
     print tab
+    print tab.cget('name', 'num')
     
 
     text="""\
-#
-#
 ##types:str	int	int
 ##defaults:none	0	0
 ##headers:0
@@ -519,8 +590,29 @@ alex	456
 mike	789	1
 """
 
-    tab = readTable(StringIO.StringIO(text))
+    tab = readTable(StringIO.StringIO(text))    
     
     print tab.defaults
     print tab
     print tab[0][1]
+
+
+    text="""\
+##types:str	int	int
+##defaults:none	0	0
+##headers:2
+#
+# hello
+#
+skip
+name	0	1
+matt	123	3
+alex	456	
+mike	789	1
+"""
+
+    tab = readTable(StringIO.StringIO(text))    
+    
+    print tab
+
+    

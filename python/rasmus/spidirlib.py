bc792c59b0b0b34046aefefd9ef8183a8bb97197
rasmus <rasmus>
2006-12-03 11:17:20 +0000

    *** empty log message ***

--- python/rasmus/spidirlib.py
+++ python/rasmus/spidirlib.py
@@ -912,6 +912,7 @@ def mleBaserate(lens, means, sdevs, baserateparam):
     ind = range(len(means))
     ind.sort(lambda a, b: cmp(means[b], means[a]))
     #ind = ind[:len(ind) / 4 + 1]
+    ind = ind[:max(4, len(means)-2)]
     means = util.mget(means, ind)
     sdevs = util.mget(sdevs, ind)
     lens = util.mget(lens, ind)
